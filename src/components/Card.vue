e01d0284f2df83d13873fe8a0a5e48cc4a461622
dcamero2 <dcamero2@stu.southtexascollege.ed>
2026-02-04 17:53:44 -0600

    Added card, changed Name

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="card">
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+.card {
+  width: min(600 px, 100%);
+  background: #e7e7ec;
+  padding: 18px;
+  border-radius: 16px;
+  box-shadow: 0 10px 24px rgba(10, 10, 32, 0.2);
+}
+</style>
